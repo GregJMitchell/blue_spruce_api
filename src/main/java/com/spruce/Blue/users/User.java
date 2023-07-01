3597c51f88eb2a9d28d0fba29f0e3726a79144b8
Greg Mitchell <g2mitchell3003@gmail.com>
2023-06-30 22:40:16 -0600

    Add validations for user entity

--- src/main/java/com/spruce/Blue/users/User.java
+++ src/main/java/com/spruce/Blue/users/User.java
@@ -1,6 +1,8 @@
 package com.spruce.Blue.users;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
 
 @Entity
 @Table(name = "users")
@@ -8,27 +10,38 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, length = 50)
+    @NotBlank(message = "User name cannot be blank")
     private String name;
+
+    @Column(nullable = false, length = 50, unique = true)
+    @NotBlank(message = "User email cannot be blank")
     private String email;
+
+    @Column(nullable = false, length = 3)
+    @Positive(message = "User age must be greater than 0")
     private int age;
 
     public User() {}
-    public User(Long id, String name, String email) {
+    public User(String name, String email, int age) {
         this.id = id;
         this.name = name;
         this.email = email;
+        this.age = age;
     }
 
     public Long getId() {
-        return this.id;
+        return id;
     }
 
     public String getName() {
-        return this.name;
+        return name;
     }
 
     public String getEmail() {
-        return this.email;
+        return email;
     }
 
+    public int getAge() { return age; }
 }
